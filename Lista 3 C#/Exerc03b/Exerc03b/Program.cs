7af279931611aead9905761d0c191907bd43e180
Thiago de Souza Rodrigues <rodriguessr02@gmail.com>
2022-09-23 16:55:10 -0300

    Exer 3 e 4
    add exer 3  e 4 completos

---
+++ Lista 3 C#/Exerc03b/Exerc03b/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerc03b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           /* Exercício 3: Escreva um programa que lei a dois valores reais e apresente a diferença do
+                maior para o menor.Um método chamado diferenca deverá ser implementado para realizar
+                o cálculo e exibir o resultado.*/
+            double n1, n2, dif;
+            Console.WriteLine("Digite um número");
+            n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro número");
+            n2 = double.Parse(Console.ReadLine());
+            dif = diferenca(n1, n2);
+            Console.Write("A diferença entre {0} e {1} é de {2}", n1, n2, dif);
+            Console.ReadKey();
+        }
+
+        static double diferenca(double num1, double num2)
+        {
+            if (num1 > num2)
+            {
+                double dif = num1 - num2;
+                return dif;
+            }
+            else
+            {
+                double dif = num2 - num1;
+                return dif;
+            }
+        }
+    }
+    }
+
---
+++ Lista 3 C#/Exerc04/Exerc04/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerc04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, C;
+            Console.WriteLine("Entre com o Valor de A");
+            A = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o Valor de B");
+            B = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o Valor de C");
+            C = double.Parse(Console.ReadLine());
+            if (A < B + C && B < A + C && C < B + C)
+            {
+                double triangulo = verificador(A, B, C);
+
+                switch (triangulo)
+                {
+                    case 1:
+                        Console.WriteLine("Triângulo do Tipo equilátero");
+                        break;
+                    case 2:
+                        Console.WriteLine("Triângulo do Tipo  isósceles");
+                        break;
+                    case 3:
+                        Console.WriteLine("Triângulo do Tipo escaleno");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não é Possivel Formar um triângulo");
+            }
+            Console.ReadKey();
+        }
+        static double verificador(double m1, double m2, double m3)
+        {
+            if(m1 == m2 && m2 == m3) {
+                double triangulo = 1;
+                return triangulo;
+            }else if(m1 == m2 || m1 == m3 || m2 == m3){
+                double triangulo = 2;
+                return triangulo;
+            }else{
+                double triangulo = 3;
+                return triangulo;
+            }
+            
+            
+        }
+    }
+}
