314b9438dc9a06ead71d81019d0b6212fdb50bdb
Thiago de Souza Rodrigues <rodriguessr02@gmail.com>
2022-09-23 12:27:04 -0300

    Lista 3
    Lista feita em sala de aula ate o exercio numero 02

---
+++ Lista 3 C#/Exerc01/Exerc01/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerc01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double nota, nota2;
+            double media;
+            Console.WriteLine("Entre Com o nome do aluno ");
+            nome = Console.ReadLine();
+
+            lernota();
+            nota = int.Parse(Console.ReadLine());
+
+            lernota();
+            nota2 = int.Parse(Console.ReadLine());
+
+            media = calcularMedia(nota, nota2);
+            if(media >= 7)
+            {
+                Console.WriteLine("Aluno {0} Aprovado, com a media {1}", nome, media);
+            }
+            else
+            {
+                Console.Write("Aluno {0} Reprovado, com a media {1}", nome, media);
+            }
+            Console.ReadKey();
+        }
+        static void lernota()
+        {
+            Console.WriteLine("Entre com as notas do aluno: ");
+
+        }
+
+        static double calcularMedia(double n, double n2)
+        {
+            double m= (n + n2) / 2;
+            return m;
+        }
+    }
+}
---
+++ Lista 3 C#/Exerc02/Exerc02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerc02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, fat;
+            Console.WriteLine("Entre com número que deseja exibir o fatorial");
+            num = int.Parse(Console.ReadLine());
+            fat = calcularFatorial(num);
+
+            Console.WriteLine("O fatorial de {0} é {1}", num, fat);
+            Console.ReadKey();
+        }
+
+        static int calcularFatorial(int n)
+        {
+            int calcFat = 1;
+           for (int i = 1; i <= n; i++)
+            {
+                calcFat = calcFat * i;
+            }
+            return calcFat;
+        }
+    }
+}
